9e76fefe06b004e929e742dfe45739bd1a58f6f3
zefanja <npm@zefanjas.de>
2018-03-21 05:27:57 +0700

    * added script to check which hosts did not fetch their command file from linbo-remote -p command -w 0 -r room

---
+++ linbo/check-offline-hosts.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import requests
+import json
+
+# CONFIG
+linbocmdPath = "/var/linbo/linbocmd"
+workstations = "/etc/linuxmuster/workstations"
+# Optional config for Mattermost
+mattermostUrl = "https://chat.cdsc.ac.th/hooks/4weetj1pzpd6ukq1z31diwds1a"
+channel = "#monitoring"
+botName = "Linbo"
+icon_url = "http://aqi.cdsc.ac.th/images/lmn.png"
+
+# Read workstations for hostname lookup
+workstationsFile = open(workstations, "r")
+devices = workstationsFile.readlines()
+
+#IP to hostname
+def lookupIP(ip):
+    for host in devices:
+        if host.find(";"+ip+";") != -1:
+            return host.split(";")[1]
+            break;
+
+# Post message to Mattermost channel
+def postMessage(message):
+    attachment = {"color": "danger", "text":  message}
+    data = {"channel": channel, "username": botName, "attachments": [attachment], "icon_url": icon_url}
+    conn = requests.post(mattermostUrl, data = json.dumps(data))
+
+# Check if there are files left from Linbo (linbo-remote -r room -p command -w 0)
+offlineHosts = sorted(os.listdir(linbocmdPath))
+numberOfHosts = len(offlineHosts)
+if numberOfHosts > 0:
+    message = ":warning: " + str(numberOfHosts) + " host(s) were offline:\n"
+    for host in offlineHosts:
+        message += "* " + lookupIP(host.replace(".cmd", "")) + "\n"
+
+        # Remove Linbo command files
+        commandFile = os.path.join(linbocmdPath, host)
+        try:
+            if os.path.isfile(commandFile):
+                os.unlink(commandFile)
+        except Exception as e:
+            print(e)
+
+    print(message)
+
+    # Post to Mattermost if a URL is configured
+    if mattermostUrl != "":
+        postMessage(message)
+else:
+    print("Everything is OK!")
